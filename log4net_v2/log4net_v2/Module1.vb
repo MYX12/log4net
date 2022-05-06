d583a74578578465aaca443feea4ee961ca794d7
MYX12 <92570565+MYX12@users.noreply.github.com>
2022-05-06 16:13:08 +0800

    Add files via upload

---
+++ log4net_v2/log4net_v2/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+    Private logger As log4net.ILog = log4net.LogManager.GetLogger(GetType(Module1).ToString)
+
+    Sub Main()
+        Try
+            logger.Info("this is start of do work")
+            Dowork()
+            logger.Info("this is the end of do work")
+        Catch ex As Exception
+            logger.Error("your app has an error")
+            logger.Error(ex.Message)
+        End Try
+
+    End Sub
+    Private Sub Dowork()
+        Console.WriteLine("this is the do work function")
+
+    End Sub
+End Module
---
+++ log4net_v2/log4net_v2/My Project/AssemblyInfo.vb
@@ -0,0 +1,36 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("log4net_v2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("log4net_v2")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: log4net.Config.XmlConfigurator(ConfigFile:="Log4Net.Config.xml", Watch:=True)>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a7a02f22-780c-4abe-b941-a817e494e47b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
